b0ded2cad5d26fdd68a232f196ad344de24df72d
Grigor Safaryan <grigor.safaryan@ekeng.am>
2026-06-22 17:12:08 +0400

    feat: invitation ssr enabled

--- src/lib/invitation/index.js
+++ src/lib/invitation/index.js
@@ -1,12 +1,14 @@
 // import { cache } from "react";
 
+import { BASE_API } from "../api/config";
+
 export const pickLang = (obj, lang = "en") =>
   obj?.[lang] || obj?.en || obj?.hy || obj?.ru || "";
 
 export async function getInvitationData(slug) {
   try {
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_DEV}invitations/url/${slug}`,
+      `${BASE_API}invitations/url/${slug}`,
       { next: { revalidate: 60 } },
     );
     if (!res.ok) return null;
@@ -20,7 +22,7 @@ export async function getInvitationData(slug) {
 // export const getInvitationData = cache(async (slug) => {
 //   try {
 //     const res = await fetch(
-//       `${process.env.NEXT_PUBLIC_API_DEV}invitations/url/${slug}`,
+//       `${BASE_API}invitations/url/${slug}`,
 //       { next: { revalidate: 60 } },
 //     );
 //     if (!res.ok) return null;
