8efb1279fe54c28822c044f115a9596f9bb36ffd
Grigor Safaryan <grigor.safaryan@ekeng.am>
2026-06-22 17:24:28 +0400

    fix: invitation lib

--- src/lib/invitation/index.js
+++ src/lib/invitation/index.js
@@ -1,14 +1,12 @@
 // import { cache } from "react";
 
-import { BASE_API } from "../api/config";
-
 export const pickLang = (obj, lang = "en") =>
   obj?.[lang] || obj?.en || obj?.hy || obj?.ru || "";
 
 export async function getInvitationData(slug) {
   try {
     const res = await fetch(
-      `${BASE_API}invitations/url/${slug}`,
+      `${process.env.NEXT_PUBLIC_API_RELEASE}invitations/url/${slug}`,
       { next: { revalidate: 60 } },
     );
     if (!res.ok) return null;
@@ -22,7 +20,7 @@ export async function getInvitationData(slug) {
 // export const getInvitationData = cache(async (slug) => {
 //   try {
 //     const res = await fetch(
-//       `${BASE_API}invitations/url/${slug}`,
+//       `${process.env.NEXT_PUBLIC_API_DEV}invitations/url/${slug}`,
 //       { next: { revalidate: 60 } },
 //     );
 //     if (!res.ok) return null;
