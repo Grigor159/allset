f74accd7f2ba25395fd7adad2b3db3dd2e154280
Grigor Safaryan <grigor.safaryan@ekeng.am>
2026-07-14 11:43:17 +0400

    style: about us mobile

--- src/components/public/about.jsx
+++ src/components/public/about.jsx
@@ -11,14 +11,32 @@ export const About = () => {
   return (
     <Stack py="80px" bg="#f6f6f7">
       <Container maxW="1440px" px={{ base: "24px", md: "40px" }}>
-        <Flex w="100%" justify={"space-between"} gap="110px" align={"center"}>
+        <Flex
+          w="100%"
+          justify={"space-between"}
+          gap={{ base: "50px", xl: "110px" }}
+          align={"center"}
+          direction={{ base: "column", xl: "row" }}
+        >
           <Stack flex="1" gap="24px">
-            <Title as="h1" fontSize="42px" text="about_title" color="#004143" />
+            <Title
+              as="h1"
+              fontSize="42px"
+              lineHeight="48px"
+              text="about_title"
+              color="#004143"
+            />
             {/* <SubTitle text="plan_subtitle" /> */}
             <Description text="about_desc" textAlign="start" />
           </Stack>
 
-          <Image src={img.src} w="590px" h="242px" alt="about-img1"   borderRadius="5px"/>
+          <Image
+            src={img.src}
+            w={{ base: "100%", xl: "590px" }}
+            h="242px"
+            alt="about-img1"
+            borderRadius="5px"
+          />
         </Flex>
       </Container>
     </Stack>
--- src/components/public/about_choose.jsx
+++ src/components/public/about_choose.jsx
@@ -3,7 +3,6 @@
 import React from "react";
 import { Stack, Container, Flex, Image } from "@chakra-ui/react";
 import { Title } from "./typography/title";
-import { SubTitle } from "./typography/subTitle";
 import { Description } from "./typography/description";
 import bg from "@/assets/imgs/planning_bg.png";
 import img2 from "@/assets/imgs/about_img2.png";
@@ -16,18 +15,24 @@ export const AboutChoose = () => {
       bgImage={`url(${bg.src})`}
       bgPos="right 40px top"
       bgRepeat={"no-repeat"}
-      bgSize={"297px"}
+      bgSize={{ base: "160px", xl: "297px" }}
       backgroundColor="#f6f6f7"
     >
       <Container maxW="1440px" px={{ base: "24px", md: "40px" }}>
-        <Flex w="100%" justify={"space-between"} align={"center"} gap="110px">
+        <Flex
+          w="100%"
+          justify={"space-between"}
+          align={"center"}
+          gap={{ base: "50px", xl: "110px" }}
+          direction={{ base: "column", xl: "row" }}
+        >
           <Stack flex="1" gap="24px">
             <Title as="h1" text="team" />
             {/* <SubTitle text="choose_desc_one" /> */}
             <Description text="team_desc" textAlign="start" />
           </Stack>
 
-          <Flex flex="1" gap={"28px"}>
+          <Flex flex="1" gap={"28px"} direction={{ base: "column", md: "row" }}>
             <Image
               src={img2.src}
               w="281px"
@@ -41,7 +46,7 @@ export const AboutChoose = () => {
               h="334px"
               alt="about-img3"
               borderRadius="5px"
-              mt="146px"
+              mt={{ base: "0", md: "146px" }}
             />
           </Flex>
         </Flex>
